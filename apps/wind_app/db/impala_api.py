6aa52750096da4eff8d3afb52f196bdd3d805cd9
Data-drone <bpl.law@gmail.com>
2020-02-18 23:50:12 +1100

    wip impala integration

---
+++ apps/wind_app/db/impala_api.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import ibis
+
+# Testing impala connection
+
+webhdfs_host = ''
+webhdfs_port = ''
+impala_host = ''
+impala_port = ''
+
+
+hdfs = ibis.hdfs_connect(host=webhdfs_host, port=webhdfs_port)
+client = ibis.impala.connect(
+    host=impala_host, port=impala_port, hdfs_client=hdfs
+)
+db = client.database('wind')
+
+
+def get_wind_data(start, end):
+
+    table = db.wind
+    filtered = table.filter([table.rowid > start,
+                            table.rowid < end])
+    df = filtered['Speed', 'SpeedError', 'Direction']
+
+
+    return df
+
+
+def get_wind_data_by_id(id):
+
+    table = db.wind
+    df = table[table.rowid=id]
+
+    return df
