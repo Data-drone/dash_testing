a2b0e6f856e5ac4b22c651b0eab06e70792fe54c
brian-law <bpl.law@gmail.com>
2020-02-21 10:53:14 -0800

    added a data loader but its pretty inefficient and pretty far from best practice

--- apps/wind_app/db/load_data.py
+++ apps/wind_app/db/load_data.py
@@ -2,13 +2,39 @@ import pathlib
 import sqlite3
 import pandas as pd
 from impala.dbapi import connect
+import sys
 
 impala_server = 'ec2-54-252-190-231.ap-southeast-2.compute.amazonaws.com'
 
 # connect to impala instance
-conn = connect(host=host, port=21050)
+conn = connect(host=impala_server, port=21050)
 cursor = conn.cursor()
 
-cur.execute(""" CREATE TABLE Wind (rowid, Speed, SpeedError, Direction)""")
+print("create table")
+
+
+#cursor.execute(""" CREATE TABLE wind (rowid INT, Speed FLOAT, SpeedError FLOAT, Direction FLOAT)""")
+
+
+### read in the data table
+### temp -we have the table in csv
+print("load df")
+df = pd.read_csv('wind_dataset_table.csv')
+
+
+# works but is really really slow and inefficient
+print("load data")
+for index, row in df.iterrows():
+
+    sql = 'INSERT INTO wind ( rowid, Speed, SpeedError, Direction ) VALUES ( {0}, {1}, {2}, {3} )'\
+                .format(int(row['index']), row['Speed'], row['SpeedError'], row['Direction'])
+
+    print(">>>>>> " + sql, file=sys.stderr, flush=True)
+
+    cursor.execute(sql)
+
+
+
+
 
 
