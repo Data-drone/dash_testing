9d45323c5d8ce186555cdf8870acd6947f7c3645
brian-law <bpl.law@gmail.com>
2020-02-29 19:18:09 -0800

    fixed the requirements to make sure we can talk to impala and opened app to be accessible but we need to update the IMPALA and HDFS location flags and have a running db. maybe we should have a page for when there is no data connection rather than just blow up

--- apps/wind_app/app.py
+++ apps/wind_app/app.py
@@ -458,4 +458,4 @@ def show_num_bins(autoValue, slider_value):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host='127.0.0.1', port='8100')
+    app.run_server(debug=True, host='0.0.0.0', port='8100')
